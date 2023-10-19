16a4fcbd5a5148e9fdbbb58bb68ef83f445bc35d
Jordan McCord <jordanmccord23@yahoo.com>
2023-10-19 14:29:46 -0400

    fixed issue with wrong credentials

--- krogercouponclipper.py
+++ krogercouponclipper.py
@@ -13,16 +13,13 @@ service = Service(executable_path="B:\\Code Projects\\chromedriver.exe")
 options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 driver = webdriver.Chrome(service=service, options=options)
 driver.get("https://www.kroger.com/savings/cl/mycoupons/")
-email = creds.email
-password = creds.password
-
 time.sleep(5)
 if(driver.current_url == 'https://www.kroger.com/signin?redirectUrl=/savings/cl/mycoupons/'):
     email_box = driver.find_element(By.ID, 'SignIn-emailInput')
-    email_box.send_keys(email)
+    email_box.send_keys(creds.email)
     time.sleep(2)
     password_box = driver.find_element(By.ID, 'SignIn-passwordInput')
-    password_box.send_keys(password)
+    password_box.send_keys(creds.password)
     
     remember_me_box = driver.find_element(By.ID, 'SignIn-rememberMe')
     time.sleep(2)
